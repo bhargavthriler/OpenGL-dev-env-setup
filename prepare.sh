da213d15809343de3b1f15767dfd334fd20df77a
Bhargav <bhargavthriler@gmail.com>
2019-08-14 09:11:04 +0530

    package glfw to install

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y cmake libx11-dev xorg-dev libgl1-mesa-glx libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libglu1-mesa libglu1-mesa-dev 
+apt install -y cmake libx11-dev xorg-dev libgl1-mesa-glx libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libglu1-mesa libglu1-mesa-dev libglfw3-dev libglfw3
 
 git clone https://github.com/Dav1dde/glad.git
 cd glad
