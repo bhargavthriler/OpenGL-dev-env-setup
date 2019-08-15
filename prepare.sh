0126310fc1f815ffe38e68a9e15fc6144fc9e363
Bhargav Das <bhargavthriler@gmail.com>
2019-08-15 11:32:20 +0530

    added command for package list update

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#update package list
+apt update
+
+#install required packages
 apt install -y cmake libx11-dev xorg-dev libgl1-mesa-glx libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libglu1-mesa libglu1-mesa-dev libglfw3-dev libglfw3
 
+#clone and setup glad
 git clone https://github.com/Dav1dde/glad.git
 cd glad
 cmake ./
